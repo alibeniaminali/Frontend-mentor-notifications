f8306fa0f2b5a076d74d889f90d59f424f3c926d
Ali Ali <alibeniaminali@gmail.com>
2023-07-30 01:09:06 +0100

    added toggle when a user has a message

--- scripts/app.js
+++ scripts/app.js
@@ -2,18 +2,28 @@ import data from '../data.json' assert { type: 'json' }
 
 function init() {
   const notificationsContent = document.querySelector('.notifications__content')
-  console.log(notificationsContent)
 
   let view = ''
   data.forEach((item) => {
     const div = `
-        <div class="notifications__content__card ${!item.read ? 'unread-background' : ''}">
+        <div class="notifications__content__card ${
+          !item.read ? 'unread-background' : ''
+        }">
           <div class="notifications__content__card__image">
-            <img src="${item.image}" alt="Picture of ${item.firstName} ${item.secondName}" />
+            <img src="${item.image}" alt="Picture of ${item.firstName} ${
+      item.secondName
+    }" />
           </div>
           <div class="notifications__content__card__content">
-            <p><a href="#" class="user-name">${item.firstName} ${item.secondName}</a> <span class="notification-type">${item.notificationType}</span> <a href="#" class="group">${item?.group}</a> <span class="${!item?.read ? 'unread' : ''}"></span></p>
+            <p><a href="#" class="user-name">${item.firstName} ${
+      item.secondName
+    }</a> <span class="notification-type">${
+      item.notificationType
+    }</span> <a href="#" class="group">${
+      item.group ? item.group : ''
+    }</a> <span class="${!item?.read ? 'unread' : ''}"></span></p>
             <p class="timestamp">${item.timestamp}</p>
+            ${item.message ? `<p class="message hidden">${item.message}</p>` : ''}     
           </div>
         </div>
     `
@@ -21,7 +31,10 @@ function init() {
   })
 
   notificationsContent.innerHTML += view
-  console.log(data)
+  const messageParagraph = document.querySelector('.message')
+  messageParagraph.parentElement.parentElement.addEventListener('click', () => {
+    messageParagraph.classList.toggle('hidden')
+  })
 }
 
 window.addEventListener('DOMContentLoaded', init)
