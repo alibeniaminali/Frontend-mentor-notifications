a7ba15eb9c1c801a018735e372ce2c5bac393c6d
Ali Ali <alibeniaminali@gmail.com>
2023-07-30 23:20:21 +0100

    fixed format

--- scripts/app.js
+++ scripts/app.js
@@ -9,33 +9,24 @@ function init() {
   let view = ''
   data.forEach((item) => {
     const div = `
-        <div class="notifications__content__card ${
-          item.actionPicture ? 'has-image' : ''
-        } ${!item.read ? 'unread-background' : ''}">
-            <div class="notifications__content__card__image">
-                <img src="${item.image}" alt="Picture of ${item.firstName} ${
-      item.secondName
-    }" />
-            </div>
-          <div class="notifications__content__card__content">
-            <div>
-                <a href="#" class="user-name pr">${item.firstName} ${
-      item.secondName
-    }</a> 
-                <span class="notification-type pr">${item.action}</span> 
-                <a href="#" class="group pr">${
-                  item.group ? item.group : ''
-                }</a> 
-                <span class="${!item.read ? 'unread' : ''}"></span>
-            </div>
-            <p class="timestamp">${item.timestamp}</p>
-            ${item.message ? `<p class="message">${item.message}</p>` : ''}     
-          </div>
-					${item.actionPicture ? `<img src="${item.actionPicture}" alt="chess" />` : ''}
+      <div class="notifications__content__card ${item.actionPicture ? 'has-image' : ''} ${!item.read ? 'unread-background' : ''}">
+        <div class="notifications__content__card__image">
+          <img src="${item.image}" alt="Picture of ${item.firstName} ${item.secondName}" />
         </div>
+        <div class="notifications__content__card__content">
+          <div>
+            <a href="#" class="user-name pr">${item.firstName} ${item.secondName}</a> 
+            <span class="notification-type pr">${item.action}</span> 
+            <a href="#" class="group pr">${item.group ? item.group : ''}</a> 
+            <span class="${!item.read ? 'unread' : ''}"></span>
+          </div>
+          <p class="timestamp">${item.timestamp}</p>
+          ${item.message ? `<p class="message">${item.message}</p>` : ''}     
+        </div>
+					${item.actionPicture ? `<img src="${item.actionPicture}" alt="chess" />` : ''}
+      </div>
     `
     view += div
-
     if (!item.read) {
       count++
     }
